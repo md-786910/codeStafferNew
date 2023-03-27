9808ae8f6686fa592157a6e44301d884b7598135
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-03-27 18:10:18 +0530

    18:10

--- src/components/Pricing/PricingTable.js
+++ src/components/Pricing/PricingTable.js
@@ -1,70 +1,150 @@
-import React from 'react'
+import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Button } from '../Button/Button';
-import "./pricing.css"
+import { Button } from "../Button/Button";
+import "./pricing.css";
+import { BsCheckCircleFill } from "react-icons/bs";
 function PricingTable() {
     return (
         <section className="pricingTable">
             <div className="wrapper">
                 <Container fluid className="p-0">
-                    <Row >
+                    <Row>
+                        <Col lg={3} className="mb-4">
+                            <div className="pricingBox1">
+                                <div className="pricingGarentyBox d-flex flex-column justify-content-center gap-4">
+                                    <span>30-Day Guarantee</span>
+                                    <span>Platform Agnostic</span>
+                                    <span>24/7 Security Team</span>
+                                </div>
+
+                                <div className="my-4 d-flex flex-column " style={{ gap: "13px" }}>
+                                    <p className=" ">Site covered</p>
+                                    <p className=" ">
+                                        Malware & hack removals by our security experts.
+                                    </p>
+                                    <p className=" ">Malware Removal SLA</p>
+                                    <p className=" ">Post-cleanup basic report</p>
+                                    <p className=" ">Frequency of advanced security scans</p>
+                                    <p className=" ">Website Application Firewall (WAF)</p>
+                                    <p className=" ">Blocklist Monitoring & Removal</p>
+                                    <p className=" ">SSL Support & Monitoring</p>
+                                    <p className=" ">
+                                        Stop Hacks (Virtual Patching/Hardening)
+                                    </p>
+                                    <p className=" ">
+                                        Firewall Protection – HTTPS & PCI compliant
+                                    </p>
+                                    <p className=" ">Advanced DDoS Mitigation</p>
+                                    <p className=" ">CDN Speed Enhancement</p>
+                                    <p className=" ">High Availability/Load Balancing</p>
+                                    <p className=" ">CMS & Hosting Compatibility</p>
+                                    <p className=" ">Support Requests</p>
+                                </div>
+
+                            </div>
+                        </Col>
                         {
                             [...new Array(3)].map((i, index) => {
                                 return (
-                                    <Col lg={4} className="mb-4" key={index}>
+                                    <Col lg={3} className="mb-4" key={index} style={{ width: "18%" }}>
                                         <div className="pricingBox">
-                                            <h1>
-                                                Pro
-                                            </h1>
-                                            <p>
-                                                Faster response and support for SSL certificates.
-                                            </p>
-
-                                            <div className="mt-3">
-                                                <span>
-                                                    Includes:
-                                                </span>
-                                                <ul className='mt-3'>
-                                                    <li>Single Website</li>
-                                                    <li>Basic Plan</li>
-                                                    <li>Advanced SSL Support</li>
-                                                    <li>12hrs Malware Removal SLA</li>
-                                                </ul>
-                                            </div>
-
-
-                                            <div className="pricingBtnDown">
-                                                <h3>
-                                                    $299.99
-                                                </h3>
-                                                <span>
-                                                    Paid Annually
-                                                </span>
-
+                                            <div className="pricingGarentyBox">
+                                                <h3 className="text-center">Basic Platform</h3>
+                                                <p className="subText mt-3">
+                                                    Perfect for bloggers and small site owners requiring
+                                                    occasional cleanups with ongoing security scans.
+                                                </p>
+                                                <h4 className="text-center">$299.99</h4>
                                                 <div className="btnBox mt-4">
-                                                    <Button fullWidth={"fullWidth"}>
-                                                        Buy Now
-                                                    </Button>
+                                                    <Button fullWidth={"fullWidth"}>Buy now</Button>
                                                 </div>
 
+                                            </div>
+                                            <div className="my-4 d-flex flex-column  align-items-center " style={{ gap: "17px" }}>
+                                                <p className="">1</p>
+                                                <p className="">unlimited</p>
+                                                <p className="">30hrs</p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
 
+                                                <p className="">Every 12 hrs</p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">
+                                                    <BsCheckCircleFill color={"green"} size={23} />
+                                                </p>
+                                                <p className="">Agnostic</p>
+                                                <p className="">Ticket</p>
                                             </div>
 
-
+                                            <div className="pricingBtnDown ">
+                                                <div className="btnBox mt-4">
+                                                    <Button fullWidth={"fullWidth"}>Buy now</Button>
+                                                </div>
+                                            </div>
                                         </div>
-
-
                                     </Col>
                                 )
                             })
                         }
-                    </Row>
+                        <Col lg={3} className="mb-4" style={{ width: "21%", }}>
+                            <div className="pricingBox" style={{ height: "199vh" }} >
+                                <div className="pricingGarentyBox">
+                                    <h3 className="text-center">Multi-site &
+                                        Custom Plans</h3>
+                                    <p className="subText mt-3">
+                                        Perfect for bloggers and small site owners requiring
+                                        occasional cleanups with ongoing security scans.
+                                    </p>
+                                    <h4 className="text-center">price upon request</h4>
+                                    <div className="btnBox mt-4">
+                                        <Button fullWidth={"fullWidth"}>Chat now</Button>
+                                    </div>
 
+                                </div>
+
+                                <div className="my-4 d-flex flex-column " style={{ gap: "13px" }}>
+                                    <h5>
+                                        Call: 1–888–873–0817
+                                    </h5>
+                                    <p className=" ">Site covered</p>
+                                    <p className=" ">Malware Removal SLA</p>
+                                    <p className=" ">Post-cleanup basic report</p>
+                                    <p className=" ">CDN Speed Enhancement</p>
+                                    <p className=" ">Support Requests</p>
+                                </div>
+
+                            </div>
+                        </Col>
+                    </Row>
                 </Container>
             </div>
-
-        </section >
-    )
+        </section>
+    );
 }
 
-export default PricingTable
+export default PricingTable;
